6a19df7aac3fe0ee355f9c87403d0c24f52d6415
rhizalfahmi <rhizall.00@gmail.com>
2018-11-21 06:41:41 +0700

    add styling to show the complete status, and add to mark todo items as completed

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -15,6 +16,11 @@ class TodoItemsController < ApplicationController
     end
     redirect_to @todo_list
   end
+  
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item Completed"
+  end
 
   private
 
@@ -22,8 +28,11 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
   end
 
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
   def todo_item_params
     params[:todo_item].permit(:content)
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_201732) do
+ActiveRecord::Schema.define(version: 2018_11_19_211350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_11_19_201732) do
     t.bigint "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
